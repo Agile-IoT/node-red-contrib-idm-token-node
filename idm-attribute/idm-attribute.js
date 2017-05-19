eca6eb2b692fdc2ec35df19856d13448f34a6c94
david <nopbyte@users.noreply.github.com>
2017-05-19 15:03:01 +0200

    include attribute node

---
+++ idm-attribute/idm-attribute.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var lo = require('lodash');
+var SDK = require('agile-sdk');
+var node_type = "idm-attribute";
+
+module.exports = function(RED) {
+    function getAtttribute(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        this.on('input', function(msg) {
+          
+          var conditions = [{cond:msg.token, msg:'msg.token not provided'}, {cond:(config && config.idm),msg:'idm url not provided in node\'s configuration'},{cond:msg.entity_id,msg:'msg.entity_id not provided'},{cond: msg.entity_type,msg: 'msg.entity_type not provided'},{cond: msg.attribute,msg: 'msg.attribute not provided'}];
+          var errors  = conditions.reduce(
+            (accumulator, currentValue, currentIndex, array) => {
+              return (!currentValue.cond)? (accumulator + currentValue.msg + ". "):accumulator;
+            },
+            ""
+          );
+          if(errors){
+            node.warn("errors in node "+node_type+"..."+ errors);
+            node.send(msg);
+          }
+          else{
+
+            var agile = SDK("",config.idm,msg.token);
+            agile.idm.entity.getEntity(msg.entity_id,msg.entity_type).then(function(entity){
+              value = lo.get(entity,msg.attribute);
+              var dest = msg.destination_property?msg.destination_property:"entity_attribute";
+              lo.set(msg,dest,value);
+              node.send(msg);
+            }).catch(function(error){
+              node.warn("IDM call not successful: "+error);
+              node.send(msg);
+            });
+          }
+        });
+    };
+
+    RED.nodes.registerType(node_type,getAtttribute, {credentials: {
+     clientId: {type:"text"},
+     clientSecret: {type:"password"}
+    }});
+};
